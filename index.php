425e1d866980549114cfe5aa7da410fedd6c9075
Hrat1 <hrat.tahmazyan99@gmail.com>
2021-02-20 16:53:24 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Online School</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">My website</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Homepage</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Blog</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
