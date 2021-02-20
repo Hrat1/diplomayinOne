a2441f9c1d079e6f0f7e0ef24d80996d9d246b73
Hrat1 <hrat.tahmazyan99@gmail.com>
2021-02-20 17:08:13 +0400

    Added header file

---
+++ header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+        <a href="#" class="navbar-brand">My website</a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Homepage</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Blog</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li clas="navbar-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
--- index.php
+++ index.php
@@ -9,31 +9,7 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-<nav class="navbar navbar-light bg-light navbar-expand-lg fixed-top">
-    <div class="container-fluid">
-        <a href="#" class="navbar-brand">My website</a>
-        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="navbar-nav ml-auto">
-                <li clas="navbar-item">
-                    <a href="#" class="nav-link">Homepage</a>
-                </li>
-                <li clas="navbar-item">
-                    <a href="#" class="nav-link">Blog</a>
-                </li>
-                <li clas="navbar-item">
-                    <a href="#" class="nav-link">About</a>
-                </li>
-                <li clas="navbar-item">
-                    <a href="#" class="nav-link">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
+<?php include_once("header.php")?>
 
 <script src="bootstrap/js/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
